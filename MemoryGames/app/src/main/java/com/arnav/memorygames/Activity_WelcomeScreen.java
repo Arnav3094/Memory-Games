7bf874123fa2e02b2e55a1acfcf18466e0f16ddb
Arnav Mangla <arnav3094pvt@gmail.com>
2021-06-09 18:14:02 +0530

    initial commit

---
+++ MemoryGames/app/src/main/java/com/arnav/memorygames/Activity_WelcomeScreen.java
@@ -0,0 +1,14 @@
+package com.arnav.memorygames;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_WelcomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+    }
+}
