6966294b845cb03236149c9db42e72c829a4f6e6
Arnav Mangla <arnav3094pvt@gmail.com>
2021-06-10 15:56:22 +0530

    Added Play Button

--- MemoryGames/app/src/main/java/com/arnav/memorygames/Activity_WelcomeScreen.java
+++ MemoryGames/app/src/main/java/com/arnav/memorygames/Activity_WelcomeScreen.java
@@ -3,8 +3,10 @@ package com.arnav.memorygames;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,6 +18,8 @@ public class Activity_WelcomeScreen extends AppCompatActivity {
     ImageView curvedWedgesRight;
 
     TextView welcomeText;
+
+    Button playButton;
 
     Animation slideInRightFadeIn;
     Animation slideInLeftFadeIn;
@@ -32,17 +36,28 @@ public class Activity_WelcomeScreen extends AppCompatActivity {
         curvedWedgeLeft = findViewById(R.id.curved_wedge_left);
         curvedWedgesRight = findViewById(R.id.curved_wedges_right);
         welcomeText = findViewById(R.id.welcomeText);
+        playButton = findViewById(R.id.playButton);
 
         // Anims
         slideInRightFadeIn = AnimationUtils.loadAnimation(this, R.anim.slide_in_right_fade_in);
         slideInLeftFadeIn = AnimationUtils.loadAnimation(this, R.anim.slide_in_left_fade_in);
         slideDownFromTop = AnimationUtils.loadAnimation(this, R.anim.slide_down_from_top);
 
+        // Setting anims
         sideCircleRight.setAnimation(slideInRightFadeIn);
         sideCircleLeft.setAnimation(slideInLeftFadeIn);
         curvedWedgesRight.setAnimation(slideInRightFadeIn);
         curvedWedgeLeft.setAnimation(slideInLeftFadeIn);
         welcomeText.setAnimation(slideDownFromTop);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //TODO: Set Animations for all elements to leave the screen when activity is changing
+
+            }
+        });
 
     }
 }
