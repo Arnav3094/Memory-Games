f617c981789f4bcc6a639fd1990b3b4c47b567f5
Arnav Mangla <arnav3094pvt@gmail.com>
2021-06-10 10:48:24 +0530

    Created slide in left and right animations And added to Activity_WelcomeScreen.java (to the Circles and wedges

--- MemoryGames/app/src/main/java/com/arnav/memorygames/Activity_WelcomeScreen.java
+++ MemoryGames/app/src/main/java/com/arnav/memorygames/Activity_WelcomeScreen.java
@@ -3,12 +3,39 @@ package com.arnav.memorygames;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class Activity_WelcomeScreen extends AppCompatActivity {
+
+    ImageView sideCircleRight;
+    ImageView sideCircleLeft;
+    ImageView curvedWedgeLeft;
+    ImageView curvedWedgesRight;
+    Animation slideInRightFadeIn;
+    Animation slideInLeftFadeIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+        // Widgets
+        sideCircleRight = findViewById(R.id.side_circle_right);
+        sideCircleLeft = findViewById(R.id.side_circle_left);
+        curvedWedgeLeft = findViewById(R.id.curved_wedge_left);
+        curvedWedgesRight = findViewById(R.id.curved_wedges_right);
+
+        // Anims
+        slideInRightFadeIn = AnimationUtils.loadAnimation(this, R.anim.slide_in_right_fade_in);
+        slideInLeftFadeIn = AnimationUtils.loadAnimation(this, R.anim.slide_in_left_fade_in);
+
+        sideCircleRight.setAnimation(slideInRightFadeIn);
+        sideCircleLeft.setAnimation(slideInLeftFadeIn);
+        curvedWedgesRight.setAnimation(slideInRightFadeIn);
+        curvedWedgeLeft.setAnimation(slideInLeftFadeIn);
+
+
     }
 }
