59bcb54e50e6964cdddfaef89b2be331e3ff09e6
Arnav Mangla <arnav3094pvt@gmail.com>
2021-06-10 15:23:21 +0530

    Added animation to Welcome! text

--- MemoryGames/app/src/main/java/com/arnav/memorygames/Activity_WelcomeScreen.java
+++ MemoryGames/app/src/main/java/com/arnav/memorygames/Activity_WelcomeScreen.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Activity_WelcomeScreen extends AppCompatActivity {
 
@@ -13,8 +14,12 @@ public class Activity_WelcomeScreen extends AppCompatActivity {
     ImageView sideCircleLeft;
     ImageView curvedWedgeLeft;
     ImageView curvedWedgesRight;
+
+    TextView welcomeText;
+
     Animation slideInRightFadeIn;
     Animation slideInLeftFadeIn;
+    Animation slideDownFromTop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +31,18 @@ public class Activity_WelcomeScreen extends AppCompatActivity {
         sideCircleLeft = findViewById(R.id.side_circle_left);
         curvedWedgeLeft = findViewById(R.id.curved_wedge_left);
         curvedWedgesRight = findViewById(R.id.curved_wedges_right);
+        welcomeText = findViewById(R.id.welcomeText);
 
         // Anims
         slideInRightFadeIn = AnimationUtils.loadAnimation(this, R.anim.slide_in_right_fade_in);
         slideInLeftFadeIn = AnimationUtils.loadAnimation(this, R.anim.slide_in_left_fade_in);
+        slideDownFromTop = AnimationUtils.loadAnimation(this, R.anim.slide_down_from_top);
 
         sideCircleRight.setAnimation(slideInRightFadeIn);
         sideCircleLeft.setAnimation(slideInLeftFadeIn);
         curvedWedgesRight.setAnimation(slideInRightFadeIn);
         curvedWedgeLeft.setAnimation(slideInLeftFadeIn);
-
+        welcomeText.setAnimation(slideDownFromTop);
 
     }
 }
