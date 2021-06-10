4a6e5105cd941638418d631784d0003a74a374f8
Arnav Mangla <arnav3094pvt@gmail.com>
2021-06-10 17:54:08 +0530

    Added OnClick for playButton but it doesn't work yet

--- MemoryGames/app/src/main/java/com/arnav/memorygames/Activity_WelcomeScreen.java
+++ MemoryGames/app/src/main/java/com/arnav/memorygames/Activity_WelcomeScreen.java
@@ -2,7 +2,9 @@ package com.arnav.memorygames;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -10,20 +12,21 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.lang.reflect.Method;
+
 public class Activity_WelcomeScreen extends AppCompatActivity {
 
-    ImageView sideCircleRight;
-    ImageView sideCircleLeft;
-    ImageView curvedWedgeLeft;
-    ImageView curvedWedgesRight;
+    private static final String TAG = "Activity_WelcomeScreen";
+
+    ImageView sideCircleRight, sideCircleLeft, curvedWedgeLeft, curvedWedgesRight;
 
     TextView welcomeText;
 
     Button playButton;
 
-    Animation slideInRightFadeIn;
-    Animation slideInLeftFadeIn;
-    Animation slideDownFromTop;
+    Animation slideInRight, slideInLeft, slideDownFromTop, slideOutToBottom;
+
+    Intent intentWelcomeToMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,20 +42,50 @@ public class Activity_WelcomeScreen extends AppCompatActivity {
         playButton = findViewById(R.id.playButton);
 
         // Anims
-        slideInRightFadeIn = AnimationUtils.loadAnimation(this, R.anim.slide_in_right_fade_in);
-        slideInLeftFadeIn = AnimationUtils.loadAnimation(this, R.anim.slide_in_left_fade_in);
+        slideInRight = AnimationUtils.loadAnimation(this, R.anim.slide_in_right);
+        slideInLeft = AnimationUtils.loadAnimation(this, R.anim.slide_in_left);
         slideDownFromTop = AnimationUtils.loadAnimation(this, R.anim.slide_down_from_top);
 
+
+
         // Setting anims
-        sideCircleRight.setAnimation(slideInRightFadeIn);
-        sideCircleLeft.setAnimation(slideInLeftFadeIn);
-        curvedWedgesRight.setAnimation(slideInRightFadeIn);
-        curvedWedgeLeft.setAnimation(slideInLeftFadeIn);
+        sideCircleRight.setAnimation(slideInRight);
+        sideCircleLeft.setAnimation(slideInLeft);
+        curvedWedgesRight.setAnimation(slideInRight);
+        curvedWedgeLeft.setAnimation(slideInLeft);
+
         welcomeText.setAnimation(slideDownFromTop);
 
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                slideOutToBottom = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_out_to_bottom);
+                // Here you will put the anims for each element
+                playButton.setAnimation(slideOutToBottom);
+                Log.d(TAG, "onClick: button clicked");
+//                curvedWedgeLeft.setAnimation();
+//                curvedWedgesRight.setAnimation();
+//                welcomeText.setAnimation();
+//                sideCircleLeft.setAnimation();
+//                sideCircleRight.setAnimation();
+
+//                Thread intentThread = new Thread(){
+//                    public void run(){
+//                        try {
+//                            Thread.sleep(2000);
+//                            startActivity(intentWelcomeToMain);
+//                            Log.d(TAG, "Thread Started");
+//                        } catch(Exception e){
+//                            Log.d(TAG, "thread run: Exception -> " + e);
+//                        }
+//                    }
+//                };
+
+/*                intentWelcomeToMain = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intentWelcomeToMain);
+
+                // This will take the actual view anims
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);*/
 
                 //TODO: Set Animations for all elements to leave the screen when activity is changing
 
