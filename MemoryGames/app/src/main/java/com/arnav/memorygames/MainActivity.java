227053c3a6c4aea929f688ee8c0169b05e0da03d
Arnav Mangla <arnav3094pvt@gmail.com>
2021-06-10 16:26:47 +0530

    Added Main Activity to Manifest

---
+++ MemoryGames/app/src/main/java/com/arnav/memorygames/MainActivity.java
@@ -0,0 +1,14 @@
+package com.arnav.memorygames;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
