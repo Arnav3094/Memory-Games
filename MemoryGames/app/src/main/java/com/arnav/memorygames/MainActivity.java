ef2709082ffed12ae289c179937ec6f81b532b27
Arnav Mangla <arnav3094pvt@gmail.com>
2021-07-04 18:57:34 +0530

    files from MainActivity/master merge

--- MemoryGames/app/src/main/java/com/arnav/memorygames/MainActivity.java
+++ MemoryGames/app/src/main/java/com/arnav/memorygames/MainActivity.java
@@ -1,14 +1,61 @@
 package com.arnav.memorygames;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    BottomNavigationView bottomNavBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialisations
+        bottomNavBar = findViewById(R.id.bottom_nav_bar);
+        TilesFragment tilesFragment = new TilesFragment();
+
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameLayout, tilesFragment).commit();
+
+        bottomNavBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                switch (item.getItemId()){
+                    case (R.id.tiles_bottom):
+                        selectedFragment = new TilesFragment();
+                        break;
+
+/*                  Add the code for the cases below =>>
+
+                    case (R.id.words_bottom):
+                        break;
+
+                    case (R.id.numbers_bottom):
+                        break;
+*/
+                }
+                assert selectedFragment != null;
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.frameLayout, selectedFragment)
+                        .commit();
+                return true;
+            }
+        });
+
     }
 }
