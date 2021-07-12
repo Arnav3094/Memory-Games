3fc276265ed2b068e7a33cdf0d821cdebb2518ca
Arnav Mangla <arnav3094pvt@gmail.com>
2021-07-12 10:54:04 +0530

    added a few arrays

--- MemoryGames/app/src/main/java/com/arnav/memorygames/TilesFragment_Game.java
+++ MemoryGames/app/src/main/java/com/arnav/memorygames/TilesFragment_Game.java
@@ -11,6 +11,20 @@ import androidx.fragment.app.Fragment;
 
 public class TilesFragment_Game extends Fragment {
 
+    final int[] tileColours = new int[] {R.color.tile_blue,R.color.tile_cyan,
+            R.color.tile_green, R.color.tile_maroon, R.color.tile_orange,
+            R.color.tile_purple, R.color.tile_red, R.color.tile_yellow};
+
+    final int[] tiles = new int[] {R.drawable.tile_blue, R.drawable.tile_cyan,
+            R.drawable.tile_green, R.drawable.tile_maroon, R.drawable.tile_orange,
+            R.drawable.tile_purple, R.drawable.tile_red, R.drawable.tile_yellow};
+
+    final int[] position = new int[]
+            {11, 12, 13, 14,
+                    21, 22, 23, 24,
+                    31, 32, 33, 34,
+                    41, 42, 43, 44};
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
