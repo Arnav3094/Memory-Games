13557a80ca5972d56869722d206629a4bf159316
Arnav Mangla <arnav3094pvt@gmail.com>
2021-07-12 10:45:31 +0530

    changed name and added tiles to the layout

--- MemoryGames/app/src/main/java/com/arnav/memorygames/TilesFragment_Start.java
+++ MemoryGames/app/src/main/java/com/arnav/memorygames/TilesFragment_Start.java
@@ -11,27 +11,34 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class TilesFragment extends Fragment {
+public class TilesFragment_Start extends Fragment {
 
 
     ImageView curView;
     int countPair;
     final int[] tileColours = new int[] {R.color.tile_blue,R.color.tile_cyan,
-            R.color.tile_green,R.color.tile_maroon,R.color.tile_orange,
-            R.color.tile_purple,R.color.tile_red,R.color.tile_yellow,};
+            R.color.tile_green, R.color.tile_maroon, R.color.tile_orange,
+            R.color.tile_purple, R.color.tile_red, R.color.tile_yellow};
 
-    final int[] position = new int[] {};
+    final int[] tiles = new int[] {R.drawable.tile_blue, R.drawable.tile_cyan,
+            R.drawable.tile_green, R.drawable.tile_maroon, R.drawable.tile_orange,
+            R.drawable.tile_purple, R.drawable.tile_red, R.drawable.tile_yellow};
 
-    public TilesFragment() {
-        // Required empty public constructor
-    }
+    final int[] position = new int[]
+            {11, 12, 13, 14,
+            21, 22, 23, 24,
+            31, 32, 33, 34,
+            41, 42, 43, 44};
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         return inflater.inflate(R.layout.fragment_tiles, container, false);
     }
 
