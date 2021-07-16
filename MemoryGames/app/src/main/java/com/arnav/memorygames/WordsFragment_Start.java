b0a5801f34afb839887fa3ab378ae36687f23429
Mehul Goel <mehulgoel678678@gmail.com>
2021-07-16 20:40:08 +0530

    Add a button to WordsFragment_Start.java

--- MemoryGames/app/src/main/java/com/arnav/memorygames/WordsFragment_Start.java
+++ MemoryGames/app/src/main/java/com/arnav/memorygames/WordsFragment_Start.java
@@ -3,6 +3,8 @@ package com.arnav.memorygames;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,7 +19,16 @@ public class WordsFragment_Start extends Fragment {
                              Bundle savedInstanceState) {
         Button startButton = getView().findViewById(R.id.startButton);
 
-
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = requireActivity().getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                WordsFragment_Display WordsDisplayFragment = new WordsFragment_Display();
+                transaction.replace(R.id.frameLayout, WordsDisplayFragment);
+                transaction.commit();
+            }
+        });
 
         return inflater.inflate(R.layout.fragment_words_start, container, false);
     }
