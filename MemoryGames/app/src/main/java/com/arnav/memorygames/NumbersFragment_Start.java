a05d133e31baa404910119477b73f065d3c87d1e
Arnav Mangla <arnav3094pvt@gmail.com>
2021-07-16 20:25:37 +0530

    added requireView() instead of getView() and requireActivity() instead of getActivity()

--- MemoryGames/app/src/main/java/com/arnav/memorygames/NumbersFragment_Start.java
+++ MemoryGames/app/src/main/java/com/arnav/memorygames/NumbersFragment_Start.java
@@ -1,21 +1,16 @@
 package com.arnav.memorygames;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
-import android.os.Handler;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
-
-import java.util.Random;
 
 public class NumbersFragment_Start extends Fragment {
     Button startButton;
@@ -29,12 +24,12 @@ public class NumbersFragment_Start extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        startButton = getView().findViewById(R.id.startButton);
+        startButton = requireView().findViewById(R.id.startButton);
 
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FragmentManager manager = getActivity().getSupportFragmentManager();
+                FragmentManager manager = requireActivity().getSupportFragmentManager();
                 FragmentTransaction transaction = manager.beginTransaction();
                 NumbersFragment_Display NumbersDisplayFragment = new NumbersFragment_Display();
                 transaction.replace(R.id.frameLayout, NumbersDisplayFragment);
