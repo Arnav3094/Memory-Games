407ce2c8767b62bc60da940e3470da6478b97849
Arnav Mangla <arnav3094pvt@gmail.com>
2021-07-18 21:42:07 +0530

    fixed up TilesFragment_Game fixed matching conditions added handler removed and and all TileButton class usages added useful methods for background res

--- MemoryGames/app/src/main/java/com/arnav/memorygames/TilesFragment_Game.java
+++ MemoryGames/app/src/main/java/com/arnav/memorygames/TilesFragment_Game.java
@@ -1,6 +1,7 @@
 package com.arnav.memorygames;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,10 +20,6 @@ public class TilesFragment_Game extends Fragment {
 
     private static final String TAG = "TilesFragment_Game";
 
-    final int[] tileColours = new int[]{R.color.tile_blue, R.color.tile_cyan,
-            R.color.tile_green, R.color.tile_maroon, R.color.tile_orange,
-            R.color.tile_purple, R.color.tile_red, R.color.tile_yellow};
-
     final int[] tileDrawables = new int[]{R.drawable.tile_blue, R.drawable.tile_cyan,
             R.drawable.tile_green, R.drawable.tile_maroon, R.drawable.tile_orange,
             R.drawable.tile_purple, R.drawable.tile_red, R.drawable.tile_yellow};
@@ -34,13 +31,13 @@ public class TilesFragment_Game extends Fragment {
     Button tile11, tile12, tile13, tile14, tile21, tile22, tile23, tile24,
             tile31, tile32, tile33, tile34, tile41, tile42, tile43, tile44;
 
+    ArrayList<Integer> tileDrawablesRes;
+
     ArrayList<Button> tiles;
     ArrayList<Button> tilesOpened;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         Log.d(TAG, "onCreateView: Moved to " + TAG);
         return inflater.inflate(R.layout.fragment_tiles_game, container, false);
     }
@@ -65,8 +62,11 @@ public class TilesFragment_Game extends Fragment {
         tile42 = requireView().findViewById(R.id.tile42);
         tile43 = requireView().findViewById(R.id.tile43);
         tile44 = requireView().findViewById(R.id.tile44);
+
         tiles = new ArrayList<>();
         tilesOpened = new ArrayList<>();
+        tileDrawablesRes = new ArrayList<>();
+
         Button[] tilesArray = new Button[]{tile11, tile12, tile13, tile14, tile21, tile22, tile23, tile24, tile31, tile32, tile33, tile34, tile41, tile42, tile43, tile44};
         tiles.addAll(Arrays.asList(tilesArray));
         Collections.shuffle(tiles);
@@ -82,16 +82,15 @@ public class TilesFragment_Game extends Fragment {
 
         for (Button button : tiles) {
             button.setOnClickListener(tileClickListener);
+            button.setBackgroundResource(R.drawable.hidden_tile);
+            tileDrawablesRes.add(R.drawable.hidden_tile);
         }
-
     }
 
-//    private void makeArrayList(Button[] array, ArrayList<Button> arrayList) {
-//        for (Button button : array) {
-//            arrayList.add(button);
-//        }
-//    }
-
+/*    TODO : Error Resolution
+        => when clicking too fast, throws an index out of bounds exception
+        => why is handler doing the stuff twice??
+    */
 
     void flipTile(Button tile) {
         int index = (int) tiles.indexOf(tile);
@@ -101,32 +100,71 @@ public class TilesFragment_Game extends Fragment {
         Log.d(TAG, "flipTile: " + index);
 
         if (tilesOpened.size() < 2) {
-            tile.setBackgroundResource(tileDrawables[index]);
-            tilesOpened.add(tile);
+            if (!tilesOpened.contains(tile)) {
+                // Opening a closed tile
+                setBackgroundRes(tile, tileDrawables[index]);
+                tilesOpened.add(tile);
+            } else {
+                // Closing an open tile
+                setBackgroundRes(tile, R.drawable.hidden_tile);
+                tilesOpened.remove(tile);
+            }
         }
 
         if (tilesOpened.size() == 2) {
-            if (match(tilesOpened)) {
-                for (Button button : tilesOpened) {
-                    button.setVisibility(View.INVISIBLE);
-                    button.setEnabled(false);
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Log.d(TAG, "run: Running the Handler");
+                    if (match(tilesOpened)) {
+                        for (Button tile : tilesOpened) {
+                            tile.setVisibility(View.INVISIBLE);
+                            tile.setEnabled(false);
+                            Log.d(TAG, "run: Tiles matched and made invisible");
+                        }
+                    } else {
+                        for (Button tile : tilesOpened) {
+                            setBackgroundRes(tile, R.drawable.hidden_tile);
+                            Log.d(TAG, "run: Tiles not matched and hidden");
+                        }
+                    }
+                    tilesOpened.clear();
                 }
-            } else {
-                for (Button button : tilesOpened) {
-                    button.setBackgroundResource(R.drawable.hidden_tile);
-                }
-            }
-            tilesOpened.clear();
+            }, 500);
         }
+        /*
+        if (tilesOpened.size() < 2) {
+            tile.setBackgroundResource(tileDrawables[index]);
+            tilesOpened.add(tile);
+        }
+        if (tilesOpened.size() == 2) {
+            if (match(tilesOpened)) {for (Button button : tilesOpened) {button.setVisibility(View.INVISIBLE); button.setEnabled(false);}
+            } else {for (Button button : tilesOpened) {button.setBackgroundResource(R.drawable.hidden_tile);}}tilesOpened.clear();}
+            */
     }
 
     boolean match(ArrayList<Button> arr) {
         boolean bool = false;
-        if ((arr.get(0).getBackground()) == (arr.get(1).getBackground())) {
-            bool = true;
-            Log.d(TAG, "match: tile Matched!");
+        if (arr.get(0) != arr.get(1)) {
+            if (getBackgroundResource(arr.get(0)) == getBackgroundResource(arr.get(1))) {
+                bool = true;
+                Log.d(TAG, "match: tile Matched!");
+            } else {
+                Log.d(TAG, "match: tile not Matched!");
+            }
+        } else {
+            Log.d(TAG, "match: same tile clicked");
         }
         return bool;
+    }
+
+    void setBackgroundRes(Button tile, int res) {
+        tile.setBackgroundResource(res);
+        tileDrawablesRes.set(tiles.indexOf(tile), res);
+    }
+
+    int getBackgroundResource(Button tile) {
+        return tileDrawablesRes.get(tiles.indexOf(tile));
     }
 
 }
