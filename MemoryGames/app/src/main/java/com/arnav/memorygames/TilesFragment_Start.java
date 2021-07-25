8c8e3da713ec51d968aec47ed87b3aa08e137728
Arnav Mangla <arnav3094pvt@gmail.com>
2021-07-25 20:38:16 +0530

    added toast

--- MemoryGames/app/src/main/java/com/arnav/memorygames/TilesFragment_Start.java
+++ MemoryGames/app/src/main/java/com/arnav/memorygames/TilesFragment_Start.java
@@ -7,9 +7,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -19,6 +22,8 @@ public class TilesFragment_Start extends Fragment {
     private static final String TAG = "TilesFragment_Start";
     Button startButton;
     CountDownTimer timer;
+    ConstraintLayout constraintLayout;
+    LinearLayout grid;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,6 +36,9 @@ public class TilesFragment_Start extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         startButton = requireView().findViewById(R.id.startButton);
+        constraintLayout = requireView().findViewById(R.id.constraintLayout);
+        grid = requireView().findViewById(R.id.grid);
+
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,6 +48,25 @@ public class TilesFragment_Start extends Fragment {
                 startButton.setBackgroundResource(R.drawable.disabled_start_button);
             }
         });
+        constraintLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: constraint Layout");
+                incorrectClick();
+            }
+        });
+        grid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: grid");
+                incorrectClick();
+            }
+        });
+    }
+
+    void incorrectClick() {
+        Log.d(TAG, "incorrectClick: ");
+        Toast.makeText(requireActivity().getBaseContext(), "Please click the 'Start' button to begin", Toast.LENGTH_SHORT).show();
     }
 
     private void countDown(long timeLeftInMillis) {
