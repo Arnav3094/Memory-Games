e5340fa4fb2c1142487e5cb8de7d18c0be58e16a
Arnav Mangla <arnav3094pvt@gmail.com>
2021-08-09 10:49:03 +0530

    Completed WinLoseDialog All methods and attributes and IDs added

--- MemoryGames/app/src/main/java/com/arnav/memorygames/WinLoseDialog.java
+++ MemoryGames/app/src/main/java/com/arnav/memorygames/WinLoseDialog.java
@@ -5,27 +5,56 @@ import android.app.AlertDialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class WinLoseDialog {
       Activity activity;
       AlertDialog dialog;
+      TextView text;
+      LottieAnimationView lottie;
+      String textStr;
+      int lottieRes;
+      ImageView close;
 
-      public WinLoseDialog(Activity activity) {
+      public WinLoseDialog(Activity activity, int lottieRes, String textStr) {
             this.activity = activity;
+            this.textStr = textStr;
+            this.lottieRes = lottieRes;
       }
 
       public void startDialog() {
             AlertDialog.Builder builder = new AlertDialog.Builder(activity);
             LayoutInflater inflater = activity.getLayoutInflater();
-            builder.setView(inflater.inflate(R.layout.win_lose_layout, null));
+            View view = inflater.inflate(R.layout.win_lose_layout, null);
+            builder.setView(view);
             builder.setCancelable(false);
 
+            lottie = view.findViewById(R.id.lottie);
+            text = view.findViewById(R.id.alertText);
+            close = view.findViewById(R.id.close);
+
+            close.setOnClickListener(new View.OnClickListener() {
+                  @Override
+                  public void onClick(View v) {
+                        dialog.dismiss();
+                  }
+            });
+            lottie.setAnimation(lottieRes);
+            text.setText(textStr);
+
             dialog = builder.create();
-            dialog.show();
             dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
       }
 
-      public void dismissDialog() {
+      public void show() {
+            dialog.show();
+      }
+
+      public void dismiss() {
             dialog.dismiss();
       }
 
