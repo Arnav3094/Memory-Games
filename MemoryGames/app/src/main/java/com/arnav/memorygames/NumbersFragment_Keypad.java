941307f64237a73d6f0dd351381c79c4a4e59532
Arnav Mangla <arnav3094pvt@gmail.com>
2021-08-14 22:18:36 +0530

    reformatted file and cleaned up Work Left: Completing the onFinish(), onWin(), onLose()

--- MemoryGames/app/src/main/java/com/arnav/memorygames/NumbersFragment_Keypad.java
+++ MemoryGames/app/src/main/java/com/arnav/memorygames/NumbersFragment_Keypad.java
@@ -7,169 +7,166 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.DialogTitle;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class NumbersFragment_Keypad extends Fragment {
-    private static final String TAG = "NumbersFragment_Keypad";
 
-    int count = 0;
-    int points = 0;
-    int correctNumber;
-    String UserNumber = "";
-    TextView timerText;
-    TextView keypadText;
-    String correctnum;
-    String check = "";
-    public CountDownTimer countDownTimer;
-    public int remainingtime = 5;
+      private static final String TAG = "NumbersFragment_Keypad";
+      public CountDownTimer countDownTimer;
+      public int remainingtime = 5;
+      long number;
+      Button b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, del;
+      Button[] keysArray;
+      ProgressBar progressBar;
+      ArrayList<Button> keys;
+      TextView progressText, keypadText;
+      CountDownTimer timer;
+      String UserNumber = "";
+      String correctnum;
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        Bundle bundle = this.getArguments();
-        correctNumber = bundle.getInt("CorrectNumber") ;
+      @Override
+      public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                               Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_numbers_keypad, container, false);
+      }
 
-        return inflater.inflate(R.layout.fragment_numbers_keypad, container, false);
-    }
+      @Override
+      public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+            number = this.getArguments().getLong("CorrectNum");
 
-        timerText = getView().findViewById(R.id.timerText);
-        keypadText = getView().findViewById(R.id.keypadText);
-        Button b1 = getView().findViewById(R.id.keypad1);
-        Button b2 = getView().findViewById(R.id.keypad2);
-        Button b3 = getView().findViewById(R.id.keypad3);
-        Button b4 = getView().findViewById(R.id.keypad4);
-        Button b5 = getView().findViewById(R.id.keypad5);
-        Button b6 = getView().findViewById(R.id.keypad6);
-        Button b7 = getView().findViewById(R.id.keypad7);
-        Button b8 = getView().findViewById(R.id.keypad8);
-        Button b9 = getView().findViewById(R.id.keypad9);
-        Button b0 = getView().findViewById(R.id.keypad0);
-        Button deleteButton = getView().findViewById(R.id.deleteButton);
+            progressText = requireView().findViewById(R.id.progressText);
+            keypadText = requireView().findViewById(R.id.keypadText);
+            progressBar = requireView().findViewById(R.id.progressBar);
+            progressText = requireView().findViewById(R.id.progressText);
 
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                UserNumber = UserNumber + "1";
-                updateKeypadText();
-            }
-        });
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                UserNumber = UserNumber + "2";
-                updateKeypadText();
-            }
-        });
-        b3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                UserNumber = UserNumber + "3";
-                updateKeypadText();
-            }
-        });
-        b4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                UserNumber = UserNumber + "4";
-                updateKeypadText();
-            }
-        });
-        b5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                UserNumber = UserNumber + "5";
-                updateKeypadText();
-            }
-        });
-        b6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                UserNumber = UserNumber + "6";
-                updateKeypadText();
-            }
-        });
-        b7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                UserNumber = UserNumber + "7";
-                updateKeypadText();
-            }
-        });
-        b8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                UserNumber = UserNumber + "8";
-                updateKeypadText();
-            }
-        });
-        b9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                UserNumber = UserNumber + "9";
-                updateKeypadText();
-            }
-        });
-        b0.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                UserNumber = UserNumber + "0";
-                updateKeypadText();
-            }
-        });
-        deleteButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (UserNumber != null && UserNumber.length() > 0) {
-                    UserNumber = UserNumber.substring(0, UserNumber.length() - 1);
-                }
-                keypadText.setText(UserNumber);
-            }
-        });
+            b1 = requireView().findViewById(R.id.keypad1);
+            b2 = requireView().findViewById(R.id.keypad2);
+            b3 = requireView().findViewById(R.id.keypad3);
+            b4 = requireView().findViewById(R.id.keypad4);
+            b5 = requireView().findViewById(R.id.keypad5);
+            b6 = requireView().findViewById(R.id.keypad6);
+            b7 = requireView().findViewById(R.id.keypad7);
+            b8 = requireView().findViewById(R.id.keypad8);
+            b9 = requireView().findViewById(R.id.keypad9);
+            b0 = requireView().findViewById(R.id.keypad0);
+            del = requireView().findViewById(R.id.deleteButton);
+            Log.d(TAG, "onViewCreated: ");
 
-        countDownTimer = new CountDownTimer(15000, 1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                remainingtime = (int) millisUntilFinished / 1000;
-                timerText.setText(String.valueOf(remainingtime));
-            }
+            keysArray = new Button[]{b0, b1, b2, b3, b4, b5, b6, b7, b8, b9};
+            Log.d(TAG, "onViewCreated: " + Arrays.toString(keysArray));
+            keys = new ArrayList<>();
+            keys.addAll(Arrays.asList(keysArray));
+            Log.d(TAG, "onViewCreated: " + keys.toString());
 
-            @Override
-            public void onFinish() {
-                timerText.setText("Time Up");
-                correctnum = "" + correctNumber;
-                if (UserNumber.equals(correctnum)) {
-                    UserNumber = "";
-                    keypadText.setText("Correct");
-                } else if (UserNumber.equals(correctnum)) {
-                    UserNumber = "";
-                    keypadText.setText("Incorrect");
-                }
-                FragmentManager manager = getActivity().getSupportFragmentManager();
-                FragmentTransaction transaction = manager.beginTransaction();
-                NumbersFragment_Start NumberFragment_Start = new NumbersFragment_Start();
-                transaction.replace(R.id.frameLayout, NumberFragment_Start);
-                transaction.commit();
+            for (int i = 0; i <= 9; i++) {
+                  keys.get(i).setOnClickListener(onClick(i));
             }
+            del.setOnClickListener(new View.OnClickListener() {
+                  @Override
+                  public void onClick(View v) {
+                        String text = (String) keypadText.getText();
+                        if (text.length() > 0) {
+                              text = text.substring(0, text.length() - 1);
+                        }
+                        keypadText.setText(text);
+                  }
+            });
 
-        };
-        countDownTimer.start();
-    }
-    public void updateKeypadText() {
-        keypadText.setText(UserNumber);
-        //if (!correctnum.equals("")) {
-        //  keypadText.setText(correctnum);
-        //}
-    }
+
+            countDown(15000, 1);
+/*            countDownTimer = new CountDownTimer(15000, 1000) {
+                  @Override
+                  public void onTick(long millisUntilFinished) {
+                        remainingtime = (int) millisUntilFinished / 1000;
+                        progressText.setText(String.valueOf(remainingtime));
+                  }
+
+                  @Override
+                  public void onFinish() {
+                        progressText.setText("Time Up");
+                        correctnum = "" + number;
+                        if (UserNumber.equals(correctnum)) {
+                              UserNumber = "";
+                              keypadText.setText("Correct");
+                        } else if (UserNumber.equals(correctnum)) {
+                              UserNumber = "";
+                              keypadText.setText("Incorrect");
+                        }
+                        FragmentManager manager = getActivity().getSupportFragmentManager();
+                        FragmentTransaction transaction = manager.beginTransaction();
+                        NumbersFragment_Start NumberFragment_Start = new NumbersFragment_Start();
+                        transaction.replace(R.id.frameLayout, NumberFragment_Start);
+                        transaction.commit();
+                  }
+
+            };
+            countDownTimer.start();*/
+      }
+
+      private void countDown(long timeLeftInMillis, int countDownInterval) {
+            timer = new CountDownTimer(timeLeftInMillis, countDownInterval) {
+                  final int timeInSeconds = (int) (timeLeftInMillis / 1000);
+                  int secondsLeft;
+                  int progress;
+                  String timeLeftText;
+
+                  @Override
+                  public void onTick(long millisUntilFinished) {
+                        progress = (int) millisUntilFinished / (countDownInterval * 10 * timeInSeconds);
+                        secondsLeft = (int) millisUntilFinished / 1000;
+                        timeLeftText = "" + (secondsLeft + 1);
+                        progressText.setText(timeLeftText);
+                        progressBar.setProgress(progress, true);
+                  }
+
+                  @Override
+                  public void onFinish() {
+                        String timeUp = "Time's Up!";
+                        progressText.setText(timeUp);
+                        if (keypadText.getText().toString().equals(Long.toString(number))) {
+                              onWin();
+                              // TODO: Add the win fragment
+                        } else {
+                              onLose();
+                              // TODO: Add the lose fragment
+                        }
+                  }
+
+            }.start();
+      }
+
+      private void onLose() {
+
+      }
+
+      private void onWin() {
+
+      }
+
+      public View.OnClickListener onClick(int num) {
+            View.OnClickListener f = new View.OnClickListener() {
+                  @Override
+                  public void onClick(View v) {
+                        addToKeypadText("" + num);
+                  }
+            };
+            return f;
+      }
+
+      public void addToKeypadText(String str) {
+            String newText = keypadText.getText().toString() + str;
+            keypadText.setText(newText);
+      }
+
 }
 
